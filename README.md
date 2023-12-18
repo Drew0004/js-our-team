77d011be85eae89dff716e72490c70b605263745
Drew0004 <drdrewgraphic@gmail.com>
2023-12-18 15:31:58 +0100

    Create stampe in console e in pagina

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membr
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 MILESTONE 0: -----------OK
 Creare l'array di oggetti con le informazioni fornite.
-MILESTONE 1:
+MILESTONE 1: -----------OK
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
+MILESTONE 2: -----------OK
 Stampare le stesse informazioni su DOM sottoforma di stringhe
 Consigli del giorno:
 Ragioniamo come sempre a step.
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
+// Creo array vuoto
 const team = [];
+let teamContainer = document.querySelector('.team');
 
+// Pushati membri del team nell'array
 team.push(addTeamMember('Wayne Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'));
 team.push(addTeamMember('Angela Caroll', 'Chief Editor' , 'angela-caroll-chief-editor.jpg'));
 team.push(addTeamMember('Walter Gordon', 'Office Manager', 'walter-gordon-office-manager.jpg'));
@@ -7,7 +10,27 @@ team.push(addTeamMember('Angela Lopez', 'Social Media Manager', 'angela-lopez-so
 team.push(addTeamMember('Scott Estrada', 'Developer', 'scott-estrada-developer.jpg'));
 team.push(addTeamMember('Barbara Ramos', 'Graphic Designer', 'barbara-ramos-graphic-designer.jpg'));
 
-console.log(team);
+// console.log(team);
+
+// Ciclo sull'array
+for (let i = 0; i < team.length; i++){
+    console.log(team[i]);
+
+    // ciclo per le key degli Object
+    for (let key in team[i]){
+        console.log (key + ':', team[i][key]);
+        teamContainer.append(team[i][key]);
+    }
+}
+
+
+
+
+
+
+
+
+
 // Funzione
 
 function addTeamMember (name, role, image){
